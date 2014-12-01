9a829ab95da2a334c7c3388493c4d12872654921
hbooth836 <heather@Heathers-Air.att.net>
2014-11-30 17:52:32 -0800

    Info Box

--- RecycleIt.js
+++ RecycleIt.js
@@ -2,16 +2,34 @@
 function initialize() {
   var myLatlng = new google.maps.LatLng(29.721286,-95.267392);
   var mapOptions = {
-    zoom: 4,
+    zoom: 10,
     center: myLatlng
-  }
+  };
+  
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  
+ 
+  var contentString = '<div id="content">'+
+      '<div id="bodyContent">'+
+      '<p><b>Texas Port Recycling</b>' +
+      '</div>'+
+      '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+      content: contentString
+
+       });
 
   var marker = new google.maps.Marker({
       position: myLatlng,
       map: map,
-      title: 'Hello World!'
+      
+     
   });
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+  });
+
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
