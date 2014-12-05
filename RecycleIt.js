b879b84bce63bcf23d89e4974bb0f5edb0821571
hbooth836 <heather@Heathers-Air.att.net>
2014-12-05 13:53:15 -0800

    Finished Project

--- RecycleIt.js
+++ RecycleIt.js
@@ -1,37 +1,35 @@
+var markers = [
+  ['Texas Port Recycling', 28.784996,-96.973633],
+  ['Rodgers Recycling Center', 28.784606,-97.045676],
+  ['Crossroads Recycling Center', 28.768472,-96.979606]
+];
 
-function initialize() {
-  var myLatlng = new google.maps.LatLng(29.721286,-95.267392);
+function initializeMaps() {
+  var myLatlng =  new google.maps.LatLng(28.784996,-96.973633);
   var mapOptions = {
-    zoom: 10,
+    zoom: 11,
     center: myLatlng
   };
   
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-  
- 
-  var contentString = '<div id="content">'+
-      '<div id="bodyContent">'+
-      '<p><b>Texas Port Recycling</b>' +
-      '</div>'+
-      '</div>';
 
-  var infowindow = new google.maps.InfoWindow({
-      content: contentString
-
-       });
-
-  var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      
-     
-  });
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.open(map,marker);
-  });
-
+    var infowindow = new google.maps.InfoWindow(), marker, i;
+    for (i = 0; i < markers.length; i++) {
+     console.log(markers[i][1], markers[i][2])
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(markers[i][1], markers[i][2]),
+            map: map
+        });
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infowindow.setContent(markers[i][0]);
+                infowindow.open(map, marker);
+            }
+        })(marker, i));
+    }
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
 
-     
+google.maps.event.addDomListener(window, 'load', initializeMaps);
+
+
